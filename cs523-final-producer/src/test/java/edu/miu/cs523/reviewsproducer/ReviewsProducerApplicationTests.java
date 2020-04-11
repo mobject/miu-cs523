f82042eb8eec6cec91d727e8c8b6f0d5251322a6
Minh Tran <mobject@gmail.com>
2020-04-11 16:08:22 -0500

    init

---
+++ cs523-final-producer/src/test/java/edu/miu/cs523/reviewsproducer/ReviewsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs523.reviewsproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
